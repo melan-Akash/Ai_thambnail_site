5ea216620dc3e0b77e31838c8ff103606fd17ba3
Melo-code <melan.akash@icloud.com>
2026-02-01 19:23:08 +0530

    second commit

--- client/src/components/AspectRatioSelection.tsx
+++ client/src/components/AspectRatioSelection.tsx
@@ -2,36 +2,41 @@ import { RectangleHorizontal, RectangleVertical, Square } from "lucide-react"
 import { aspectRatios, type AspectRatio } from "../assets/assets"
 import { button } from "motion/react-client";
 
+const AspectRatioSelection = ({ value, onChange }: { value: AspectRatio; onchange: (ratio: AspectRatio) => void }) => {
 
-const AspectRatioSelection = ({value, onChange} : {value:AspectRatio; onchange: (ratio: AspectRatio)=> void}) => {
-
-    const iconMap = {
-        '16:9' : <RectangleHorizontal className="size-6"/>,
-        '1:1' : <Square className="size-6"/>,
-        '9:16' : <RectangleVertical className="size-6"/>,
-    } as Record<AspectRatio, React.ReactNode>
+  const iconMap = {
+    '16:9': <RectangleHorizontal className="size-6" />,
+    '1:1': <Square className="size-6" />,
+    '9:16': <RectangleVertical className="size-6" />,
+  } as Record<AspectRatio, React.ReactNode>
 
   return (
-    
+    <div className="space-y-3">
+      <label className="block text-sm font-medium text-zinc-300">
+        Aspect Ratio
+      </label>
 
-    <div className="space-y-3 dark">
-        <label className="block text-sm font-medium text-zinc-200" > Aspect Ratio</label>
+      <div className="flex gap-3">
+        {aspectRatios.map((ratio) => {
+          const selected = value === ratio
 
-        <div className="">
-            {aspectRatios.map((ratio)=>{
-                const selected = value === ratio;
-
-                return (
-                    <button key={ratio} type="button" onClick={()=>onchange(ratio)}
-                    className={`flex items-center gap-2 rounded-md border px-5 py-2.5 text-sm transition border-white/10 ${selected ? 'bg-white/10': 'hover:bg-white'}`}>
-                        {iconMap[ratio]}
-                        <span className="tracking-widest">{ratio}</span>
-                    </button>
-                )
-            })}
-
-        </div>
-      
+          return (
+            <button
+              key={ratio}
+              type="button"
+              onClick={() => onchange(ratio)}
+              className={`flex items-center gap-2 rounded-lg border px-5 py-2.5 text-sm transition-all
+                ${selected
+                  ? 'border-white/20 bg-white/10 text-white'
+                  : 'border-white/10 text-zinc-300 hover:bg-white/5 hover:border-white/20'
+                }`}
+            >
+              {iconMap[ratio]}
+              <span className="tracking-widest">{ratio}</span>
+            </button>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,92 +1,214 @@
+// import { useState } from "react";
+// import { useParams } from "react-router-dom"
+// import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
+// import SoftBackdrop from "../components/SoftBackdrop";
+// import { button } from "motion/react-client";
+// import AspectRatioSelection from "../components/AspectRatioSelection";
+// import StyleSelector from "../components/StyleSelector";
+
+
+// const Generate = () => {
+
+//     const {id} = useParams();
+//     const [title, setTitle] = useState('');
+//     const [additionalDetails, setAdditionalDetails] = useState('');
+//     const [thumbnail, setThumbnail] = useState<IThumbnail  | null>(null);
+//     const [loading , setLoading] = useState(false)
+
+//     const [aspectRatios ,setAspectRatios] = useState<AspectRatio>('16:9')
+//     const [colorSchemeId ,setColorSchemeId] = useState<string>(colorSchemes[0].id)
+//     const [style ,setStyle ] = useState<ThumbnailStyle>('Bold & Graphic' )
+
+
+//   return (
+//     <>
+//     <SoftBackdrop />
+//     <div className="pt-24 min-h-screen ">
+//         <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-28 lg:pb-8">
+//             <div className="grid lg:grid-cols-[400px_1fr] gap-0">
+//                 {/* left panel */}
+//                 <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
+//                     <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
+//                     <div>
+//                         <h2 className="text-xl font-bold text-zinc-100 mb-1">Create Your Thumbnail</h2>
+//                         <p className="text-sm text-zinc-400">Describe your vission and let ai bring it to live.</p>
+//                     </div>
+
+//                     <div className="space-y-5">
+//                         {/* title input */}
+//                         <div>
+//                             <label className="block text-sm font-medium ">
+//                                 Title or Topic
+//                             </label>
+//                             <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} maxLength={100}
+//                              placeholder="e.g., 10 Tips for Better Sleep" className="w-full px-4 py-3 rounded-lg border border-white/12 bg-black/20 
+//                              text-zinc-100 placeholder:text-zinc-400 focus:ring-2 focus:ring-pink-500"/>
+//                              <div className="flex justify-end">
+//                                 <span className="text-xs text-zinc-400">{title.length}/100</span>
+//                              </div>
+//                         </div>
+
+//                         {/* AspectRatioSelection */}
+//                         <AspectRatioSelection  value={aspectRatios} onchange={setAspectRatios}/>
+//                         {/* Style selector */}
+//                         <StyleSelector  value={style} onChange={setStyle} isOpen={styleDropdownOpen} setIsOpen={setStyleDropdownOpen}/>
+//                         {/* Color scheme selector */}
+//                         {/* Additional details */}
+
+
+//                         <div className="space-y-2" >
+//                             <label className="block text-sm font-medium" >Additional pompts <span>(optional)</span></label>
+//                             <textarea value={additionalDetails} onChange={(e)=>setAdditionalDetails(e.target.value)} rows={3}
+//                             placeholder="Add any specific alyment mood, or style perferrance.........." 
+//                             className="w-full px-4 py-3 rounded-lg border border-white/10 bg-white/6 text-zinc-100
+//                             placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500 resize-none"   ></textarea>
+
+//                         </div>
+
+//                     </div>
+
+//                     {/* button */}
+//                     {!id && (
+//                         <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600
+//                          hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
+//                             {loading ? 'Generating.....' : "Generate Thumbnail"}
+//                         </button>
+//                     )}
+
+//                     </div>
+
+//                 </div>
+//                 {/* Right panel */}
+//                 <div></div>
+
+//             </div>
+
+//         </main>
+
+//     </div>
+
+//     </>
+//   )
+// }
+
+// export default Generate
+
 import { useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
 import SoftBackdrop from "../components/SoftBackdrop";
-import { button } from "motion/react-client";
 import AspectRatioSelection from "../components/AspectRatioSelection";
-
+import StyleSelector from "../components/StyleSelector";
+import ColorSchemeSelector from "../components/ColorSchemeSelector";
 
 const Generate = () => {
 
-    const {id} = useParams();
-    const [title, setTitle] = useState('');
-    const [additionalDetails, setAdditionalDetails] = useState('');
-    const [thumbnail, setThumbnail] = useState<IThumbnail  | null>(null);
-    const [loading , setLoading] = useState(false)
+  const { id } = useParams();
 
-    const [aspectRatios ,setAspectRatios] = useState<AspectRatio>('16:9')
-    const [colorSchemeId ,setColorSchemeId] = useState<string>(colorSchemes[0].id)
-    const [style ,setStyle ] = useState<ThumbnailStyle>('Bold & Grapic' )
+  const [title, setTitle] = useState('');
+  const [additionalDetails, setAdditionalDetails] = useState('');
+  const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null);
+  const [loading, setLoading] = useState(false);
 
+  const [aspectRatios, setAspectRatios] = useState<AspectRatio>('16:9');
+  const [colorSchemeId, setColorSchemeId] = useState<string>(colorSchemes[0].id);
+  const [style, setStyle] = useState<ThumbnailStyle>('Bold & Graphic');
+
+  // ✅ MISSING STATE (FIX)
+  const [styleDropdownOpen, setStyleDropdownOpen] = useState(false);
 
   return (
     <>
-    <SoftBackdrop />
-    <div className="pt-24 min-h-screen ">
+      <SoftBackdrop />
+
+      <div className="pt-24 min-h-screen ">
         <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-28 lg:pb-8">
-            <div className="grid lg:grid-cols-[400px_1fr] gap-0">
-                {/* left panel */}
-                <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
-                    <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
-                    <div>
-                        <h2 className="text-xl font-bold text-zinc-100 mb-1">Create Your Thumbnail</h2>
-                        <p className="text-sm text-zinc-400">Describe your vission and let ai bring it to live.</p>
+          <div className="grid lg:grid-cols-[400px_1fr] gap-0">
+
+            {/* left panel */}
+            <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
+              <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
+
+                <div>
+                  <h2 className="text-xl font-bold text-zinc-100 mb-1">Create Your Thumbnail</h2>
+                  <p className="text-sm text-zinc-400">Describe your vission and let ai bring it to live.</p>
+                </div>
+
+                <div className="space-y-5">
+
+                  {/* title input */}
+                  <div>
+                    <label className="block text-sm font-medium ">
+                      Title or Topic
+                    </label>
+                    <input
+                      type="text"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                      maxLength={100}
+                      placeholder="e.g., 10 Tips for Better Sleep"
+                      className="w-full px-4 py-3 rounded-lg border border-white/12 bg-black/20 
+                      text-zinc-100 placeholder:text-zinc-400 focus:ring-2 focus:ring-pink-500"
+                    />
+                    <div className="flex justify-end">
+                      <span className="text-xs text-zinc-400">{title.length}/100</span>
                     </div>
+                  </div>
 
-                    <div className="space-y-5">
-                        {/* title input */}
-                        <div>
-                            <label className="block text-sm font-medium ">
-                                Title or Topic
-                            </label>
-                            <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} maxLength={100}
-                             placeholder="e.g., 10 Tips for Better Sleep" className="w-full px-4 py-3 rounded-lg border border-white/12 bg-black/20 
-                             text-zinc-100 placeholder:text-zinc-400 focus:ring-2 focus:ring-pink-500"/>
-                             <div className="flex justify-end">
-                                <span className="text-xs text-zinc-400">{title.length}/100</span>
-                             </div>
-                        </div>
+                  {/* AspectRatioSelection */}
+                  {/* ✅ FIX: onchange → onChange */}
+                  <AspectRatioSelection value={aspectRatios} onChange={setAspectRatios} />
 
-                        {/* AspectRatioSelection */}
-                        <AspectRatioSelection />
-                        {/* Style selector */}
-                        {/* Color scheme selector */}
-                        {/* Additional details */}
+                  {/* Style selector */}
+                  <StyleSelector
+                    value={style}
+                    onChange={setStyle}
+                    isOpen={styleDropdownOpen}
+                    setIsOpen={setStyleDropdownOpen}
+                  />
 
+                  {/* color slector */}
 
-                        <div className="space-y-2" >
-                            <label className="block text-sm font-medium" >Additional pompts <span>(optional)</span></label>
-                            <textarea value={additionalDetails} onChange={(e)=>setAdditionalDetails(e.target.value)} rows={3}
-                            placeholder="Add any specific alyment mood, or style perferrance.........." 
-                            className="w-full px-4 py-3 rounded-lg border border-white/10 bg-white/6 text-zinc-100
-                            placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500 resize-none"   ></textarea>
+                  <ColorSchemeSelector value={colorSchemeId} onChange={setColorSchemeId} />
 
-                        </div>
-
-                    </div>
-
-                    {/* button */}
-                    {!id && (
-                        <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600
-                         hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
-                            {loading ? 'Generating.....' : "Generate Thumbnail"}
-                        </button>
-                    )}
-
-                    </div>
+                  {/* Additional details */}
+                  <div className="space-y-2">
+                    <label className="block text-sm font-medium">
+                      Additional pompts <span>(optional)</span>
+                    </label>
+                    <textarea
+                      value={additionalDetails}
+                      onChange={(e) => setAdditionalDetails(e.target.value)}
+                      rows={3}
+                      placeholder="Add any specific alyment mood, or style perferrance.........."
+                      className="w-full px-4 py-3 rounded-lg border border-white/10 bg-white/6 text-zinc-100
+                      placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500 resize-none"
+                    />
+                  </div>
 
                 </div>
-                {/* Right panel */}
-                <div></div>
 
+                {/* button */}
+                {!id && (
+                  <button
+                    className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600
+                    hover:from-pink-700 disabled:cursor-not-allowed transition-colors"
+                  >
+                    {loading ? 'Generating.....' : "Generate Thumbnail"}
+                  </button>
+                )}
+
+              </div>
             </div>
 
+            {/* Right panel */}
+            <div></div>
+
+          </div>
         </main>
-
-    </div>
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Generate
+export default Generate;
