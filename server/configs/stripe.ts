028c4b221485cc70067b869d00121f1a0f1991e4
Melo-code <melan.akash@icloud.com>
2026-06-09 19:41:24 +0530

    last srtipe fix

--- server/configs/stripe.ts
+++ server/configs/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import "dotenv/config";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-02-24.acacia",
+  apiVersion: "2026-05-27.dahlia" as any,
 });
 
 export default stripe;
